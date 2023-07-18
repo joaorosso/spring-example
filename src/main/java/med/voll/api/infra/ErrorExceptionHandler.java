de52562ad3110aee499e8d6d673b3bc2bd365e25
João Rosso <joaorosso92@gmail.com>
2023-07-18 17:24:14 -0300

    feat: add ValidationMassages and create ExceptionHandler

---
+++ src/main/java/med/voll/api/infra/ErrorExceptionHandler.java
@@ -0,0 +1,30 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity error400(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ErrorMessage::new).toList());
+    }
+
+    private record ErrorMessage(String field, String message) {
+        public ErrorMessage(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+
+    }
+}
